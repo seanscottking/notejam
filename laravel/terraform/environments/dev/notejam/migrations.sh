530f1f92149192a994664e42f6e1dbf93e767b3e
Sean S. King <seanscottking@gmail.com>
2018-11-05 12:42:52 -0500

    Reorganizing

--- laravel/terraform/environments/dev/notejam/migrations.sh
+++
@@ -1,6 +0,0 @@
-aws ecs run-task \
---launch-type FARGATE \
---cluster notejam-dev \
---task-definition notejam-dev \
---network-configuration '{ "awsvpcConfiguration": { "subnets": ["subnet-0d05170d6a7f01f5e"],"securityGroups": ["sg-0a0924f579ac4b0de"],"assignPublicIp": "DISABLED" }}' \
---overrides '{ "containerOverrides": [{ "name": "notejam-dev", "command": ["php","artisan","migrate","--force"] } ]}'
